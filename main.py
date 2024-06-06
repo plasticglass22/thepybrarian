e4072176cb7b6198c423842bcacf809f5d64c94b
plasticglass <pglasstic22@protonmail.com>
2024-06-06 18:00:12 -0400

    started working with glob

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import sys
+import os
+import glob
 import re
 import numpy
+
+## get name of CWD
+
+cwd_name = os.getcwd()
+print("current location: ", cwd_name)
+
+## get names of objects (files and/or subdirs) in CWD
+cwd_contents = glob.glob('./**/*.*', recursive=True)
+for item in cwd_contents:
+    print("item: ", str(item))
