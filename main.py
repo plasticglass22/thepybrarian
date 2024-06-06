3a116012464085b0ac250edf1b7e8e4ebc792e25
plasticglass <pglasstic22@protonmail.com>
2024-06-06 19:08:33 -0400

    more work on utils.py - listing funcs

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import os
 import glob
 import re
 import numpy
+import utils
 
 ## get name of CWD
 
 cwd_name = os.getcwd()
 print("current location: ", cwd_name)
 
+utils.spacer(1)
+
 ## get names of objects (files and/or subdirs) in CWD
 ## simple get and print for a first step
-cwd_contents_rec = glob.glob('./**/*', recursive=True)
-for item in cwd_contents_rec:
-    print("item: ", str(item))
+## DELETE THIS BLOCK: MADE REDUNDANT BY UTILS.FULLCONTENTS()
+# cwd_contents_rec = glob.glob('./**/*', recursive=True)
+# for item in cwd_contents_rec:
+#     print("item: ", str(item))
 
-## try to treat files and subdirs differently
-## 
+## mess around with the new fullContents() func
+
+# utils.fullContents(cwd_name)
+## this givess too much info, because os.getcwd() gets FULL path
+
+utils.fullContents(".")
+## this is the way to go i think, doesn't expose dir names above cwd
+
+utils.spacer(2)
+
+utils.contents(".")
--- utils.py
+++ utils.py
@@ -1,4 +1,16 @@
 import re
+import sys
+import os
+import glob
+import numpy
+
+## if these exist in a result string, don't print it.
+EXCLUDE_PATTERNS = ["__pycache__"]
+
+## strip these off of a result string for formatting and presentation
+STRIP_PATTERNS = {
+    "leading_dot" : "^."
+}
 
 def spacer(lines: int) -> None:
     """
@@ -6,7 +18,41 @@ def spacer(lines: int) -> None:
 
     :param: lines - integer value specifying number of blank lines to print
     """
+    
     i = 1
     while i <= lines:
         print("")
         i += 1
+
+def exclude() -> None:
+    """
+    Exclude certain items from the printed results. For use in contents() and fullContents().
+    """
+
+    
+
+    raise NotImplementedError("exclude() is not implemented")
+
+def fullContents(dir: str) -> None:
+    """
+    Recursively lists the contents of the specified directory, and it's subdirectories.
+
+    :param: dir: str -> directory to begin recursive operation
+    """
+
+    glob_pattern = dir + "/**/*"
+    rec_contents = glob.glob(glob_pattern, recursive=True)
+    for item in rec_contents:
+        print("item: ", str(item))
+
+def contents(dir: str) -> None:
+    """
+    Lists the contents of just the directory specified as an argument.
+
+    :param: dir: str -> the directory to print
+    """
+
+    glob_pattern = dir + "/*"
+    dir_contents = glob.glob(glob_pattern)
+    for item in dir_contents:
+        print("item: ", str(item))
