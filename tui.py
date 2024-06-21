90c936dd7d2c3fc4d495824edec66ff364f8e407
plasticglass <pglasstic22@protonmail.com>
2024-06-21 10:45:24 -0400

    06212402

--- tui.py
+++ tui.py
@@ -4,7 +4,26 @@ import sys
 import utils
 import aesth
 import os
-import npyscreen
+import npyscreen as nps
 
+# class PybrarianForm(nps.Form):
+#     def create(self):
+#         self.title = self.add(nps.FixedText, name = 'Pybrarian')
+#         self.searchterm = self.add(nps.TitleText, name = 'Search For: ', value = 'Enter Search Term')
+#         tb_cwd = self.add(nps.TitleFixedText, name = "Current Working Directory: ", value = str(os.getcwd()))
 
+#     def afterEditing(self):
+#         self.parentApp.setNextForm(None)
 
+# class Pybrarian(nps.NPSAppManaged):
+#     def onStart(self):
+#         self.addForm('MAIN', PybrarianForm, name = 'Pybrarian') ## title at top of form
+
+def search(*args) -> list[str, list[tuple[str, list[str]]]]:
+     results = []
+     form = nps.Form(name = "Test Form")
+     search_input = form.add(nps.TitleText, name = "Search Term: ", value = '')
+     form.edit()
+     results.append(str(search_input.value))
+     results.append(utils.fullContents('.', search_input.value, False))
+     return results
--- utils.py
+++ utils.py
@@ -3,11 +3,13 @@ import sys
 import os
 from collections import defaultdict
 import aesth
+import npyscreen as nps
 
 ## if these exist in a result string, don't print it.
 EXCLUDE_PATTERNS = ["__pycache__", "git", ".vscode"]
 
-def fullContents(dir: str, searchterm: str, diropt: bool) -> None:
+# def fullContents(dir: str, searchterm: str, diropt: bool) -> None:
+def fullContents(dir: str, searchterm: str, diropt: bool) -> list[tuple[str, list[str]]]:
     """
     Recursively lists the contents of the specified directory and its subdirectories.
 
@@ -63,10 +65,12 @@ def fullContents(dir: str, searchterm: str, diropt: bool) -> None:
     ## because they have the same parent dir.
     ITER_ITEMS = list(sorted(CONTENTS.items()))
 
-    if diropt == False:
-        printResults(ITER_ITEMS, searchterm, False)
-    elif diropt == True:
-        printResults(ITER_ITEMS, searchterm, True)
+    return ITER_ITEMS
+
+    # if diropt == False:
+    #     printResults(ITER_ITEMS, searchterm, False)
+    # elif diropt == True:
+    #     printResults(ITER_ITEMS, searchterm, True)
 
 def directorySearch(dir: str, searchterm: str) -> None:
     """
@@ -121,6 +125,70 @@ def directorySearch(dir: str, searchterm: str) -> None:
     printResults(ITER_ITEMS, searchterm, True)
 
 def printResults(contents: list[tuple[str, list[str]]], searchterm: str, diropt: bool) -> None:
+    """
+    Prints the results of the query to STDOUT.
+
+    This functionality was initially part of fullContents(), but was moved out for portability.
+
+    :param: contents: list[tuple[str, list[str]]] -> full contents of directory and sub directories.
+    :param: searchterm: str -> search term supplied as CLI argument.
+    :param: diropt: bool -> if True, searches by directory name.
+    """
+    if diropt != True:
+        if searchterm != "":
+            for i in range(0, len(contents)):
+                levels = contents[i][0].count("/")
+                fnames = []
+                dnames = []             
+                for j in range(0, len(contents[i][1])):
+                    if searchterm.lower() in contents[i][1][j].lower():
+                        dnames.append(contents[i][0])
+                        fnames.append(contents[i][1][j])
+                    else:
+                        pass
+                dnames = list(set(dnames))
+                for dname in dnames:
+                    print("")
+                    print(aesth.colorstyle.DIRHEADER + dname + aesth.colorstyle.NORMAL)
+                    print("")
+                    for fname in fnames:
+                        print(aesth.colorstyle.GREEN + " \u2517" + levels*"\u2501\u2501 " + fname)
+            fnames.clear()
+            dnames.clear()
+        else:
+            for i in range(0, len(contents)):
+                levels = contents[i][0].count("/")
+                print("")
+                print(aesth.colorstyle.DIRHEADER + contents[i][0] + aesth.colorstyle.NORMAL) ## PRINTING DIRECTORY NAME
+                print("")
+                for j in range(0, len(contents[i][1])):
+                    print(aesth.colorstyle.GREEN + " \u2517" + levels*"\u2501\u2501 " + contents[i][1][j]) ## PRINTING FILENAME
+    elif diropt == True:
+        if searchterm != "":
+            for i in range(0, len(contents)):
+                levels = contents[i][0].count("/")
+                if searchterm.lower() in contents[i][0].lower():
+                    print("")
+                    print(aesth.colorstyle.DIRHEADER + contents[i][0] + aesth.colorstyle.NORMAL)
+                    print("")
+                for j in range(0, len(contents[i][1])):
+                    if searchterm.lower() in contents[i][0].lower():
+                        print(aesth.colorstyle.GREEN + " \u2517" + levels*"\u2501\u2501 " + contents[i][1][j])
+                    else:
+                        pass
+        else:
+            for i in range(0, len(contents)):
+                levels = contents[i][0].count("/")
+                print("")
+                print(aesth.colorstyle.DIRHEADER + contents[i][0] + aesth.colorstyle.NORMAL) ## PRINTING DIRECTORY NAME
+                print("")
+                for j in range(0, len(contents[i][1])):
+                    print(aesth.colorstyle.GREEN + " \u2517" + levels*"\u2501\u2501 " + contents[i][1][j]) ## PRINTING FILENAME
+
+    aesth.spacer(1)
+
+
+def printResultsForm(contents: list[tuple[str, list[str]]], searchterm: str, diropt: bool) -> None:
     """
     Prints the results of the query to STDOUT.
 
